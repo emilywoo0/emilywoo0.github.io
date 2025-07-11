a35f2617b4317da2ed8f251e64e1778bf2e6520b
Emily Woo <emilywoo246@gmail.com>
2025-07-11 12:30:54 -0700

    added LandingPage.js

---
+++ src/components/LandingPage.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function LandingPage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+export default LandingPage;
